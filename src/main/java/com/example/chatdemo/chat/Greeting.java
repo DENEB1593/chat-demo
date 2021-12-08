8653b540e9b749265ba6127a3e105a04f7e96e6e
leekangwoon <deneb1593@gmail.com>
2021-12-08 12:44:20 +0900

    최초 커밋

---
+++ src/main/java/com/example/chatdemo/chat/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.chatdemo.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
